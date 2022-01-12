5f47dea5efa1daa738bca8f0c85c22a658696605
Mathilde <mat.Grimal@gmail.com>
2022-01-12 15:04:49 +0100

    fix procfile and app port

--- dist/main.js
+++ dist/main.js
@@ -13,7 +13,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api', app, document);
-    await app.listen(3001);
+    await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
