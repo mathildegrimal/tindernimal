527f0b516b4bda94bbb9862ca8d81527b28ed275
Mathilde <mat.Grimal@gmail.com>
2022-01-12 14:05:23 +0100

    add dist for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,26 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {AuthModule} from './auth/auth.module';
-import {UsersService} from './users/users.service';
-import {UsersModule} from './users/users.module';
-import {AnimalsModule} from './animals/animals.module';
-import {MongooseModule} from '@nestjs/mongoose';
-import {OwnerModule} from './owner/owner.module';
-import {ConfigModule} from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { AnimalsModule } from './animals/animals.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OwnerModule } from './owner/owner.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({isGlobal: true}),
-        AuthModule,
-        OwnerModule,
-        UsersModule,
-        AnimalsModule,
-        MongooseModule.forRoot(
-            `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}${process.env.DB_HOST}/myFirstDatabase?retryWrites=true&w=majority`
-        ),
-    ],
-    controllers: [AppController],
-    providers: [AppService, UsersService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    OwnerModule,
+    UsersModule,
+    AnimalsModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}${process.env.DB_HOST}/myFirstDatabase?retryWrites=true&w=majority`,
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService, UsersService],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const config = new DocumentBuilder()
-        .setTitle('Tindernimal Api')
-        .setDescription('The tindernimal API with CRUD')
-        .setVersion('1.0')
-        .addBearerAuth(
-            {type: 'http', scheme: 'bearer', bearerFormat: 'JWT'},
-            'access-token',
-        )
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-    await app.listen(3001);
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Tindernimal Api')
+    .setDescription('The tindernimal API with CRUD')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3001);
 }
 
 bootstrap();
