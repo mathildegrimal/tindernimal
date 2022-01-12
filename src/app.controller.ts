8070da0668c1ae1935ca15d8a5addf9b3d14f315
Mathilde <mat.Grimal@gmail.com>
2022-01-12 10:14:40 +0100

    little fixes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,33 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Request,
+  Post,
+  UseGuards,
+  Body,
+} from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from './users/create-user.dto';
 
 @Controller()
+@ApiBearerAuth()
 @ApiTags('Users')
 export class AppController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  async login(@Body() user: CreateUserDto) {
+    return this.authService.login(user);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
+    console.log(req);
     return req.user;
   }
 }
