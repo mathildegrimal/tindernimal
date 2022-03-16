03663f89127228d2364428733074c86554ac6999
Mathilde <mat.Grimal@gmail.com>
2022-03-16 22:36:07 +0100

    removed dist and check package.json for class validator

--- dist/animals/dto/create-animal.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateAnimalDto {
-    readonly name: string;
-    readonly age: number;
-    readonly breed: string;
-    readonly loof: boolean;
-    readonly owner: string;
-}
--- dist/users/create-user.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateUserDto {
-    readonly username: string;
-    readonly password: string;
-}
